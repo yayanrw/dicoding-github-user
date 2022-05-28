ce6b3faa01be0939a9f88e7e4749ceca5598ed59
yayanrw <yayanraw@gmail.com>
2022-05-28 23:48:07 +0700

    add users reponse

---
+++ app/src/main/java/com/example/githubuser/model/UsersResponse.kt
@@ -0,0 +1,27 @@
+package com.example.githubuser.model
+
+data class UsersResponse(
+	val UsersResponse: List<UsersResponseItem>
+)
+
+data class UsersResponseItem(
+	val gistsUrl: String,
+	val reposUrl: String,
+	val followingUrl: String,
+	val starredUrl: String,
+	val login: String,
+	val followersUrl: String,
+	val type: String,
+	val url: String,
+	val subscriptionsUrl: String,
+	val receivedEventsUrl: String,
+	val avatarUrl: String,
+	val eventsUrl: String,
+	val htmlUrl: String,
+	val siteAdmin: Boolean,
+	val id: Int,
+	val gravatarId: String,
+	val nodeId: String,
+	val organizationsUrl: String
+)
+
