a0cd9b8f78139b8954ecdeea4a860e989bab3912
yayanrw <yayanraw@gmail.com>
2022-05-28 18:57:21 +0700

    init

--- app/src/main/java/com/example/githubuser/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.githubuser
+package com.example.githubuser.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.githubuser.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
