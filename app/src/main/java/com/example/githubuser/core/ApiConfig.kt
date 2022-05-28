8c1c22039d1a14bf0c6cce07fb8abd9fffc68247
yayanrw <yayanraw@gmail.com>
2022-05-28 23:56:54 +0700

    build config check

--- app/src/main/java/com/example/githubuser/core/ApiConfig.kt
+++ app/src/main/java/com/example/githubuser/core/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.example.githubuser.core
 
+import com.example.githubuser.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,8 +9,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
-            val loggingInterceptor =
+            val loggingInterceptor = if(BuildConfig.DEBUG) {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build()
