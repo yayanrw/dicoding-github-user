c5f5ab1ad9d52f50dc9ced4a56b9fd9749455d29
yayanrw <yayanraw@gmail.com>
2022-06-01 16:12:22 +0700

    add serach endpoint

--- app/src/main/java/com/example/githubuser/core/ApiService.kt
+++ app/src/main/java/com/example/githubuser/core/ApiService.kt
@@ -1,10 +1,12 @@
 package com.example.githubuser.core
 
 import com.example.githubuser.model.UserDetailResponse
+import com.example.githubuser.model.UserSearchResponse
 import com.example.githubuser.model.UsersResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("users")
@@ -14,4 +16,9 @@ interface ApiService {
     fun fetchUserDetail(
         @Path("login") login: String
     ): Call<UserDetailResponse>
+
+    @GET("search/users")
+    fun fetchUserSearch(
+        @Query("q") query: String
+    ): Call<UserSearchResponse>
 }
