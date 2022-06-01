1ed1178d79d713d8b03d8c2f4017c1007e7b4d2d
yayanrw <yayanraw@gmail.com>
2022-06-01 09:36:28 +0700

    tuneup model

--- app/src/main/java/com/example/githubuser/model/UsersResponse.kt
+++ app/src/main/java/com/example/githubuser/model/UsersResponse.kt
@@ -3,23 +3,57 @@ package com.example.githubuser.model
 import com.google.gson.annotations.SerializedName
 
 data class UsersResponse(
-    @SerializedName("login") var login: String? = null,
-    @SerializedName("id") var id: Int? = null,
-    @SerializedName("node_id") var nodeId: String? = null,
-    @SerializedName("avatar_url") var avatarUrl: String? = null,
-    @SerializedName("gravatar_id") var gravatarId: String? = null,
-    @SerializedName("url") var url: String? = null,
-    @SerializedName("html_url") var htmlUrl: String? = null,
-    @SerializedName("followers_url") var followersUrl: String? = null,
-    @SerializedName("following_url") var followingUrl: String? = null,
-    @SerializedName("gists_url") var gistsUrl: String? = null,
-    @SerializedName("starred_url") var starredUrl: String? = null,
-    @SerializedName("subscriptions_url") var subscriptionsUrl: String? = null,
-    @SerializedName("organizations_url") var organizationsUrl: String? = null,
-    @SerializedName("repos_url") var reposUrl: String? = null,
-    @SerializedName("events_url") var eventsUrl: String? = null,
-    @SerializedName("received_events_url") var receivedEventsUrl: String? = null,
-    @SerializedName("type") var type: String? = null,
-    @SerializedName("site_admin") var siteAdmin: Boolean? = null
+    @SerializedName("login")
+    val login: String? = null,
+    
+    @SerializedName("id")
+    val id: Int? = null,
 
+    @SerializedName("node_id")
+    val nodeId: String? = null,
+
+    @SerializedName("avatar_url")
+    val avatarUrl: String? = null,
+
+    @SerializedName("gravatar_id")
+    val gravatarId: String? = null,
+
+    @SerializedName("url")
+    val url: String? = null,
+
+    @SerializedName("html_url")
+    val htmlUrl: String? = null,
+
+    @SerializedName("followers_url")
+    val followersUrl: String? = null,
+
+    @SerializedName("following_url")
+    val followingUrl: String? = null,
+
+    @SerializedName("gists_url")
+    val gistsUrl: String? = null,
+
+    @SerializedName("starred_url")
+    val starredUrl: String? = null,
+
+    @SerializedName("subscriptions_url")
+    val subscriptionsUrl: String? = null,
+
+    @SerializedName("organizations_url")
+    val organizationsUrl: String? = null,
+
+    @SerializedName("repos_url")
+    val reposUrl: String? = null,
+
+    @SerializedName("events_url")
+    val eventsUrl: String? = null,
+
+    @SerializedName("received_events_url")
+    val receivedEventsUrl: String? = null,
+
+    @SerializedName("type")
+    val type: String? = null,
+
+    @SerializedName("site_admin")
+    val siteAdmin: Boolean? = null
 )
