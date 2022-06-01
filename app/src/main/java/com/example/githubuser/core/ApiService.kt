20d8d063f7d6a4aeeef9c7ca1d997c6870c87299
yayanrw <yayanraw@gmail.com>
2022-06-01 10:53:10 +0700

    add fetchuser endpoint with arg

--- app/src/main/java/com/example/githubuser/core/ApiService.kt
+++ app/src/main/java/com/example/githubuser/core/ApiService.kt
@@ -4,11 +4,14 @@ import com.example.githubuser.model.UserDetailResponse
 import com.example.githubuser.model.UsersResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("users")
     fun fetchUsers(): Call<ArrayList<UsersResponse>>
 
     @GET("users/{login}")
-    fun fetchDetailUser(): Call<UserDetailResponse>
+    fun fetchUserDetail(
+        @Path("login") login: String
+    ): Call<UserDetailResponse>
 }
