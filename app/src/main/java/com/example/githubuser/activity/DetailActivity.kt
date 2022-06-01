2bd3b287d8ae976e22f841cff9c4ba3ace0726b9
yayanrw <yayanraw@gmail.com>
2022-06-01 11:08:52 +0700

    load detail user from api

--- app/src/main/java/com/example/githubuser/activity/DetailActivity.kt
+++ app/src/main/java/com/example/githubuser/activity/DetailActivity.kt
@@ -1,11 +1,17 @@
 package com.example.githubuser.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.navArgs
-import com.example.githubuser.R
+import com.bumptech.glide.Glide
+import com.example.githubuser.core.ApiConfig
 import com.example.githubuser.databinding.ActivityDetailBinding
-import com.example.githubuser.databinding.ActivityMainBinding
+import com.example.githubuser.model.UserDetailResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
@@ -17,5 +23,53 @@ class DetailActivity : AppCompatActivity() {
 
         val args: DetailActivityArgs by navArgs()
         binding.tvLogin.text = args.login
+
+        getUserDetail(args.login)
+    }
+
+    private fun getUserDetail(login: String) {
+        showLoading(true)
+        val client = ApiConfig.getApiService().fetchUserDetail(login)
+        client.enqueue(object : Callback<UserDetailResponse?> {
+            override fun onResponse(
+                call: Call<UserDetailResponse?>,
+                response: Response<UserDetailResponse?>
+            ) {
+                showLoading(false)
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        setUi(responseBody)
+                    }
+                } else {
+                    showToast(response.message())
+                }
+            }
+
+            override fun onFailure(call: Call<UserDetailResponse?>, t: Throwable) {
+                showToast(t.message.toString())
+            }
+        })
+    }
+
+    private fun setUi(responseBody: UserDetailResponse) {
+        binding.tvLogin.text = responseBody.login
+        binding.tvName.text = responseBody.name
+        Glide.with(this@DetailActivity)
+            .load(responseBody.avatarUrl)
+            .circleCrop()
+            .into(binding.imgAvatar)
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    private fun showToast(messages: String) {
+        Toast.makeText(this@DetailActivity, messages, Toast.LENGTH_SHORT).show()
     }
 }
