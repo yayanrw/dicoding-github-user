728569ec64e060d3155b62ab1f8913974578fe2f
yayanrw <yayanraw@gmail.com>
2022-06-01 09:41:41 +0700

    add detail user endpoint

--- app/src/main/java/com/example/githubuser/core/ApiService.kt
+++ app/src/main/java/com/example/githubuser/core/ApiService.kt
@@ -1,10 +1,14 @@
 package com.example.githubuser.core
 
+import com.example.githubuser.model.UserDetailResponse
 import com.example.githubuser.model.UsersResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("users")
-    fun fetchUsers (): Call<ArrayList<UsersResponse>>
+    fun fetchUsers(): Call<ArrayList<UsersResponse>>
+
+    @GET("users/{login}")
+    fun fetchDetailUser(): Call<UserDetailResponse>
 }
