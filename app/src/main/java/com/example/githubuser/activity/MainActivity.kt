2b2d1cb73987a6b3b4b5d32adf88470f39d8880d
yayanrw <yayanraw@gmail.com>
2022-06-02 08:45:24 +0700

    custom appbar

--- app/src/main/java/com/example/githubuser/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/activity/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.githubuser.activity
 
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.githubuser.R
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setActionBar()
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun setActionBar() {
-        supportActionBar!!.elevation = 0.0F
+        supportActionBar?.apply {
+            setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.midnight_blue_800, theme)))
+            elevation = 0.0F
+        }
     }
 }
