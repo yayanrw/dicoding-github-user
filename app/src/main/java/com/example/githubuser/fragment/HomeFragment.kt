4a8811766dd295ad999a8c37bc423b271b1d1ef5
yayanrw <yayanraw@gmail.com>
2022-06-02 08:28:03 +0700

    remove toast

--- app/src/main/java/com/example/githubuser/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/githubuser/fragment/HomeFragment.kt
@@ -34,7 +34,6 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        showToast("Holo")
         getUsers()
     }
 
