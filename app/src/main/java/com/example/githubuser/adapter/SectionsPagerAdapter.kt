3c870d97d73676f61b17ea67546ace6427779854
yayanrw <yayanraw@gmail.com>
2022-06-03 15:28:49 +0700

    add SectionsPagerAdapter.kt

---
+++ app/src/main/java/com/example/githubuser/adapter/SectionsPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.githubuser.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.githubuser.fragment.FollowersFragment
+import com.example.githubuser.fragment.FollowingFragment
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = FollowersFragment()
+            1 -> fragment = FollowingFragment()
+        }
+        return fragment as Fragment
+    }
+}
