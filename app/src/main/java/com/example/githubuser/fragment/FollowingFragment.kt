6fa2646385cd1e18a3825b5ea26914c10656612b
yayanrw <yayanraw@gmail.com>
2022-06-03 15:28:40 +0700

    add fragment

---
+++ app/src/main/java/com/example/githubuser/fragment/FollowingFragment.kt
@@ -0,0 +1,22 @@
+package com.example.githubuser.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.githubuser.R
+
+class FollowingFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_following, container, false)
+    }
+}
