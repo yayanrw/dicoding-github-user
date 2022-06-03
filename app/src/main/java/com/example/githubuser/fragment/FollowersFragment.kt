87271671135bf51e8146dbc2897e9159b9af1156
yayanrw <yayanraw@gmail.com>
2022-06-03 16:45:28 +0700

    followers fragment

--- app/src/main/java/com/example/githubuser/fragment/FollowersFragment.kt
+++ app/src/main/java/com/example/githubuser/fragment/FollowersFragment.kt
@@ -4,19 +4,91 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.githubuser.R
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.githubuser.adapter.UserAdapter
+import com.example.githubuser.core.ApiConfig
+import com.example.githubuser.databinding.FragmentFollowersBinding
+import com.example.githubuser.model.UsersResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FollowersFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentFollowersBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var userAdapter: UserAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_followers, container, false)
+    ): View {
+        _binding = FragmentFollowersBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getFollowers("ayogatot")
+    }
+
+    private fun getFollowers(login: String) {
+        showLoading(true)
+        val client = ApiConfig.getApiService().fetchFollowers(login)
+        client.enqueue(object : Callback<ArrayList<UsersResponse>> {
+            override fun onResponse(
+                call: Call<ArrayList<UsersResponse>>,
+                response: Response<ArrayList<UsersResponse>>
+            ) {
+                showLoading(false)
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        setupRV(responseBody)
+                    }
+                } else {
+                    showToast(response.message())
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<UsersResponse>>, t: Throwable) {
+                showLoading(false)
+                showToast(t.message.toString())
+            }
+        })
+    }
+
+    private fun setupRV(listGithubUser: ArrayList<UsersResponse>?) {
+        UserAdapter(null)
+        binding.rvGithubUsers.layoutManager = LinearLayoutManager(context)
+        binding.rvGithubUsers.setHasFixedSize(true)
+        userAdapter = UserAdapter(listGithubUser)
+        binding.rvGithubUsers.adapter = userAdapter
+
+        userAdapter.setOnItemClickCallback(object : UserAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: UsersResponse) {
+//                val toDetailActivity = HomeFragmentDirections.actionHomeFragmentToDetailActivity()
+//                toDetailActivity.login = data.login!!
+//                NavHostFragment.findNavController(this@FollowersFragment).navigate(toDetailActivity)
+            }
+        })
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+            binding.rvGithubUsers.visibility = View.GONE
+        } else {
+            binding.progressBar.visibility = View.GONE
+            binding.rvGithubUsers.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showToast(messages: String) {
+        Toast.makeText(context, messages, Toast.LENGTH_SHORT).show()
     }
 }
