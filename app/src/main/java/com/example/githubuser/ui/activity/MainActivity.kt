ffe6f234ab886e7a5eabb8dd9204a818b43946d1
yayanrw <yayanraw@gmail.com>
2022-06-12 12:21:58 +0700

    remove bottom

--- app/src/main/java/com/example/githubuser/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/ui/activity/MainActivity.kt
@@ -6,19 +6,30 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.githubuser.R
+import com.example.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setActionBar()
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
     private fun setActionBar() {
         supportActionBar?.apply {
-            setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.midnight_blue_800, theme)))
+            setBackgroundDrawable(
+                ColorDrawable(
+                    resources.getColor(
+                        R.color.midnight_blue_800,
+                        theme
+                    )
+                )
+            )
             elevation = 0.0F
         }
     }
