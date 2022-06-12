4225d52be7f5f2ef36c048810b04f584335c1a77
yayanrw <yayanraw@gmail.com>
2022-06-12 10:57:00 +0700

    add FavoriteUsersDao.kt

---
+++ app/src/main/java/com/example/githubuser/database/FavoriteUsersDao.kt
@@ -0,0 +1,19 @@
+package com.example.githubuser.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface FavoriteUsersDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(favoriteUsers: FavoriteUsers)
+
+    @Update
+    fun update(favoriteUsers: FavoriteUsers)
+
+    @Delete
+    fun delete(favoriteUsers: FavoriteUsers)
+
+    @Query("SELECT * FROM favorite_users ORDER BY login ASC")
+    fun getAllFavoriteUsers(): LiveData<List<FavoriteUsers>>
+}
