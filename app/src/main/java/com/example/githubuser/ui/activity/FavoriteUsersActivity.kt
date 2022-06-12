a89ddbf76e439719f805bff357d8fb1b96636066
yayanrw <yayanraw@gmail.com>
2022-06-12 13:18:38 +0700

    favorite act

--- app/src/main/java/com/example/githubuser/ui/activity/FavoriteUsersActivity.kt
+++ app/src/main/java/com/example/githubuser/ui/activity/FavoriteUsersActivity.kt
@@ -1,12 +1,44 @@
 package com.example.githubuser.ui.activity
 
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.githubuser.R
+import com.example.githubuser.databinding.ActivityFavoriteUsersBinding
 
 class FavoriteUsersActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFavoriteUsersBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favorite_users)
+        binding = ActivityFavoriteUsersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setActionBar()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setActionBar() {
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            setBackgroundDrawable(
+                ColorDrawable(
+                    resources.getColor(
+                        R.color.midnight_blue_800,
+                        theme
+                    )
+                )
+            )
+            title = getString(R.string.favorite_users)
+            elevation = 0.0F
+        }
     }
 }
