d294ac2e8e0dd5b8f41a20a56439c7975cd04401
yayanrw <yayanraw@gmail.com>
2022-06-12 10:45:43 +0700

    add FavoriteUsers.kt class

---
+++ app/src/main/java/com/example/githubuser/database/FavoriteUsers.kt
@@ -0,0 +1,24 @@
+package com.example.githubuser.database
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity
+@Parcelize
+data class FavoriteUsers(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0,
+
+    @ColumnInfo(name = "login")
+    var login: String? = null,
+
+    @ColumnInfo(name = "type")
+    var type: String? = null,
+
+    @ColumnInfo(name = "avatar_url")
+    var avatar_url: String? = null
+) : Parcelable
