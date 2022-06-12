10cd9cfb2d9c2c9a880eeb37a2b1a9728791ab74
yayanrw <yayanraw@gmail.com>
2022-06-12 15:25:32 +0700

    check mode

--- app/src/main/java/com/example/githubuser/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/ui/activity/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.githubuser.ui.activity
 
+import android.content.Context
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.example.githubuser.R
+import com.example.githubuser.core.SettingPreferences
 import com.example.githubuser.databinding.ActivityMainBinding
+import com.example.githubuser.viewmodel.SettingViewModel
+import com.example.githubuser.viewmodel.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +28,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setActionBar()
+
+        val pref = SettingPreferences.getInstance(dataStore)
+        val settingViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
+            SettingViewModel::class.java
+        )
+
+        settingViewModel.getThemeSettings().observe(
+            this
+        ) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
