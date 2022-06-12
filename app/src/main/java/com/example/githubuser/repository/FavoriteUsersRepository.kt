24c7213229f4f415770358d56327a9587b7cb42b
yayanrw <yayanraw@gmail.com>
2022-06-12 11:21:46 +0700

    add FavoriteUsersRepository.kt

---
+++ app/src/main/java/com/example/githubuser/repository/FavoriteUsersRepository.kt
@@ -0,0 +1,29 @@
+package com.example.githubuser.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.example.githubuser.database.FavoriteUsers
+import com.example.githubuser.database.FavoriteUsersDao
+import com.example.githubuser.database.FavoriteUsersRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class FavoriteUsersRepository(application: Application) {
+    private val mFavoriteUsersDao: FavoriteUsersDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val db = FavoriteUsersRoomDatabase.getDatabase(application)
+        mFavoriteUsersDao = db.favoriteUsersDao()
+    }
+
+    fun getAllFavoriteUsers(): LiveData<List<FavoriteUsers>> = mFavoriteUsersDao.getAllFavoriteUsers()
+
+    fun insert(favoriteUsers: FavoriteUsers) {
+        executorService.execute { mFavoriteUsersDao.insert(favoriteUsers) }
+    }
+
+    fun delete(favoriteUsers: FavoriteUsers) {
+        executorService.execute { mFavoriteUsersDao.delete(favoriteUsers) }
+    }
+}
