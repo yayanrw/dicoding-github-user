5eabc8a8f938952086914730b38745add376feb9
yayanrw <yayanraw@gmail.com>
2022-06-12 17:27:46 +0700

    init viewmodel on DetailActivity.kt

--- app/src/main/java/com/example/githubuser/ui/activity/DetailActivity.kt
+++ app/src/main/java/com/example/githubuser/ui/activity/DetailActivity.kt
@@ -1,79 +1,39 @@
 package com.example.githubuser.ui.activity
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.ColorDrawable
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.navArgs
 import com.bumptech.glide.Glide
 import com.example.githubuser.R
 import com.example.githubuser.adapter.SectionsPagerAdapter
-import com.example.githubuser.core.SettingPreferences
 import com.example.githubuser.databinding.ActivityDetailBinding
 import com.example.githubuser.model.UserDetailResponse
 import com.example.githubuser.viewmodel.DetailViewModel
-import com.example.githubuser.viewmodel.SettingViewModel
-import com.example.githubuser.viewmodel.ViewModelFactory
 import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityDetailBinding
     private val detailViewModel: DetailViewModel by viewModels()
-    private lateinit var settingViewModel: SettingViewModel
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.imgbOpenOnGithub.setOnClickListener(this)
-
         val args: DetailActivityArgs by navArgs()
         setViewPager()
+        setActionBar(args.login)
 
-        detailViewModel.isLoading.observe(this) {
-            showLoading(it)
-        }
-        detailViewModel.errorMsg.observe(this) {
-            showToast(it)
-        }
-        detailViewModel.userDetail.observe(this) {
-            it?.let { it1 -> setUi(it1) }
-        }
-
-        val pref = SettingPreferences.getInstance(dataStore)
-        settingViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
-            SettingViewModel::class.java
-        )
-
-        settingViewModel.getThemeSettings().observe(
-            this
-        ) { isDarkModeActive: Boolean ->
-            if (isDarkModeActive) {
-                setActionBar(args.login, true)
-            } else {
-                setActionBar(args.login, false)
-            }
-        }
-
-        detailViewModel.getUserDetail(args.login)
+        binding.imgbOpenOnGithub.setOnClickListener(this)
+        initViewModel(args.login)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -91,30 +51,10 @@ class DetailActivity : AppCompatActivity(), View.OnClickListener {
         return super.onOptionsItemSelected(item)
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun setActionBar(login: String, isDarkMode: Boolean) {
+    private fun setActionBar(login: String) {
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
-            if (isDarkMode) {
-                setBackgroundDrawable(
-                    ColorDrawable(
-                        resources.getColor(
-                            R.color.midnight_blue_800,
-                            theme
-                        )
-                    )
-                )
-            } else {
-                setBackgroundDrawable(
-                    ColorDrawable(
-                        resources.getColor(
-                            R.color.amethyst_700,
-                            theme
-                        )
-                    )
-                )
-            }
             title = login
             elevation = 0.0F
         }
@@ -123,6 +63,19 @@ class DetailActivity : AppCompatActivity(), View.OnClickListener {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
+    }
+
+    private fun initViewModel(login: String) {
+        detailViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+        detailViewModel.errorMsg.observe(this) {
+            showToast(it)
+        }
+        detailViewModel.userDetail.observe(this) {
+            it?.let { it1 -> setUi(it1) }
+        }
+        detailViewModel.getUserDetail(login)
     }
 
     private fun setViewPager() {
