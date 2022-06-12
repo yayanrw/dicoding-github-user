49f64df395e533f7867f5c1246e7ed5ff4bc8b39
yayanrw <yayanraw@gmail.com>
2022-06-12 14:36:13 +0700

    SettingActivity.kt

--- app/src/main/java/com/example/githubuser/ui/activity/SettingActivity.kt
+++ app/src/main/java/com/example/githubuser/ui/activity/SettingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.githubuser.ui.activity
 
+import android.content.Context
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
@@ -7,11 +8,19 @@ import android.widget.CompoundButton
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.example.githubuser.R
+import com.example.githubuser.core.SettingPreferences
 import com.example.githubuser.databinding.ActivitySettingBinding
+import com.example.githubuser.viewmodel.SettingViewModel
+import com.example.githubuser.viewmodel.ViewModelFactory
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingBinding
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,23 @@ class SettingActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setActionBar()
+
+        val pref = SettingPreferences.getInstance(dataStore)
+        val settingViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
+            SettingViewModel::class.java
+        )
+
+        settingViewModel.getThemeSettings().observe(
+            this
+        ) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switchTheme.isChecked = false
+            }
+        }
 
         binding.switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
             if (isChecked) {
